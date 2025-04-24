abaf4fcb23bbbcb7da22346a7f3b49a0133a12b3
crAshly <149060523+crashly321@users.noreply.github.com>
2025-04-25 00:06:39 +0300

    fix path to sound

--- prokect_destroer_minecrafters/play_sound.py
+++ prokect_destroer_minecrafters/play_sound.py
@@ -2,7 +2,9 @@ import winsound
 import time
 
 def play_alarm():
-    winsound.PlaySound('C:/Users/malim/AppData/Roaming/mine/bu_ispugalsa.wav', winsound.SND_FILENAME)
+    base_path = os.environ.get("APPDATA")
+    script_path = os.path.join(base_path, "mine")
+    winsound.PlaySound(os.path.join(script_path, 'bu_ispugalsa.wav'), winsound.SND_FILENAME)
 
 if __name__ == "__main__":
     while True:
